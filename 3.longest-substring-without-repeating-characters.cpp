f4101ecc9664411ea9d8e2caa09e24c20df19a0d
Laksito Pamilih <laksito@protonmail.com>
2024-09-19 19:23:15 +0700

    cpp problem 2 and 3

---
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            int result = 0;
+            std::string current = "";
+    
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.at(i);
+                int index = current.find(c);
+    
+                if (index != std::string::npos) {
+                    if (result < current.length()) {
+                        result = current.length();
+                    }
+                    
+                    current = current.substr(index + 1) + c;
+                } else {
+                    current += c;
+                }
+    
+                if (i == s.length() - 1) {
+                    if (current.length() > result) result = current.length();
+                }
+    
+            }
+    
+            return result;
+        }
+    };
