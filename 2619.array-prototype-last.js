79acf62feb3ca3cb0d1db6bed55a4edf6f63f971
Laksito Pamilih <laksito@protonmail.com>
2024-09-27 12:27:15 +0700

    prob 2619

---
+++ 2619.array-prototype-last.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/array-prototype-last/
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    return this.length ? this[this.length - 1] : -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
