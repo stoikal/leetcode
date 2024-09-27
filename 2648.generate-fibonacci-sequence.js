aabc24edb86c24a37049e75e86ba0d19d0ce024d
Laksito Pamilih <laksito@protonmail.com>
2024-09-27 13:28:46 +0700

    prob 2648

---
+++ 2648.generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/generate-fibonacci-sequence/
+
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let curr = 0;
+    let next = 1;
+
+    while (true) {
+        yield curr;
+        [curr, next] = [next, curr + next];
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
