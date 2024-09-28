b9e4154613c8601cb138d44f1eebe7d6522eaa2d
Laksito Pamilih <laksito@protonmail.com>
2024-09-28 11:58:32 +0700

    prob 2704

---
+++ 2704.to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/to-be-or-not-to-be
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (x) => {
+            if (x === val) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: (x) => {
+            if (x !== val) return true;
+            throw new Error("Equal");
+        },
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
