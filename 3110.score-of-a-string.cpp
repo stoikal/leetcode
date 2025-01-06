709f773ad831a81ac4aa23d48dfdb53f2d346676
Laksito Pamilih <laksito@protonmail.com>
2025-01-06 14:26:57 +0700

    prob 3110

---
+++ 3110.score-of-a-string.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/score-of-a-string/
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+        for (int i = 1; i < s.length(); i++) {
+            sum += abs(s[i - 1] - s[i]);
+        }
+        return sum;
+    }
+};
