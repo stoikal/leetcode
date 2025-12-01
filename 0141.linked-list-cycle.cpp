bb9d45581c17588a866bf185328d88ae17bd5291
Laksito Pamilih <laksito@protonmail.com>
2025-12-01 18:20:45 +0700

    prob 141 optimize

--- 0141.linked-list-cycle.cpp
+++ 0141.linked-list-cycle.cpp
@@ -33,3 +33,33 @@ public:
         return false;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution2 {
+public:
+    bool hasCycle(ListNode *head) {
+        std::vector<ListNode*> tracker; 
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            if (slow == fast) {
+                return true;
+            }
+
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return false;
+    }
+};
