710688831601ee5b2538a6c54078cad8c41375b8
Laksito Pamilih <laksito@protonmail.com>
2026-01-02 18:08:56 +0700

    prob 338

---
+++ 0338.counting-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result;
+
+        for (int i = 0; i <= n; ++i) {
+            int temp = i;
+            int bits = 0;
+
+            while (temp) {
+                bits += temp & 1;
+                temp >>= 1;
+            }
+
+            result.push_back(bits);
+        }
+
+        return result;
+    }
+};
