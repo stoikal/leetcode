b5f7fcc5a5a2634591c54bd80241dd24f3621c0d
Laksito Pamilih <laksito@protonmail.com>
2026-01-03 21:59:17 +0700

    prob 190

---
+++ 0190.reverse-bits.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reverse-bits
+
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            res <<= 1;
+            res |= (n >> i) & 1;
+        }
+
+        return res;
+    }
+};
